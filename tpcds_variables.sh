502c81e38b22c1af9531c90c151a1c397d53c4a1
Ryan Wei <rainmanwy@gmail.com>
2024-06-20 16:41:50 +0800

    change default chip type to x86

--- tpcds_variables.sh
+++ tpcds_variables.sh
@@ -4,7 +4,7 @@ export BENCH_ROLE="dsbench"
 export SCHEMA_NAME="tpcds"
 export GREENPLUM_PATH=$GPHOME/greenplum_path.sh
 ## Set chip type to arm or x86 to avoid compiling TPC-DS tools from source code.
-export CHIP_TYPE="arm"
+export CHIP_TYPE="x86"
  
 ## Default port used is configed via env setting of $PGPORT for user $ADMIN_USER
 ## Confige the port to connect for miltiuser test if you want to use connection pools.
